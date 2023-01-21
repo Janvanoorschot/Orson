2a6f4e780f6024280ab68e971ddbf1fc6519ef8d
Jan van Oorschot <janvanoorschot@gmail.com>
2023-01-21 09:00:01 +0100

    Going back to stock pika. sigh.

--- python/orson/view/message_queue.py
+++ python/orson/view/message_queue.py
@@ -1,32 +1,23 @@
-import asyncio
 import json
 import logging
-import aio_pika
+import pika
 from orson import CHAT_EXCHANGE_NAME, UPDATES_EXCHANGE_NAME, ROOM_ANNOUNCEMENT
 
 
 class MessageQueue:
     url: str
-    connection: aio_pika.connection.AbstractConnection
-    channel: aio_pika.channel.AbstractChannel
-    exchange: aio_pika.exchange.AbstractExchange
 
     def __init__(self, url="amqp://orson:orson@127.0.0.1:8001/%2F"):
         self.url = url
-        self.loop = None
+        connection = pika.BlockingConnection(pika.URLParameters(url=self.url))
+        channel = connection.channel()
+        exchange = channel.exchange_declare(exchange=CHAT_EXCHANGE_NAME, exchange_type='topic')
+        connection.close()
 
     def send(self, room_name, message):
-        if self.loop is None:
-            self.loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(self.loop)
-        self.loop.run_until_complete(self.a_send(room_name, message))
-
-    async def a_send(self, room_name, message):
-        connection = await aio_pika.connect_robust(self.url)
-        channel = await connection.channel()
-        exchange = await channel.declare_exchange(CHAT_EXCHANGE_NAME, 'topic')
-        await exchange.publish(
-            aio_pika.Message(body=json.dumps(message).encode()),
-            routing_key=room_name
-        )
-        await connection.close()
+        connection = pika.BlockingConnection(pika.URLParameters(url=self.url))
+        channel = connection.channel()
+        body = json.dumps(message, default=str)
+        properties = pika.BasicProperties(delivery_mode=2)
+        channel.basic_publish(exchange=CHAT_EXCHANGE_NAME, routing_key=room_name, body=body, properties=properties)
+        connection.close()
